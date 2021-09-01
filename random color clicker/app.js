c1ff72915eadb96a988f4b704527d26cb8cd5f82
Adriana <am20dipi@alum.siena.edu>
2021-09-01 17:26:35 -0400

    rendering browser title and button after each click

--- random color clicker/app.js
+++ random color clicker/app.js
@@ -2,6 +2,7 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.querySelector(".color-button");
 const color = document.querySelector(".color");
+const title = document.querySelector('.title')
 
 btn.addEventListener("click", function () {
   let hexColor = "#";
@@ -9,9 +10,12 @@ btn.addEventListener("click", function () {
     hexColor += hex[getRandomNumber()];
   }
 
+  document.body.appendChild(title)
+  document.body.appendChild(btn)
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
-  document.body.appendChild(btn)
+  
+
 });
 
 function getRandomNumber() {
