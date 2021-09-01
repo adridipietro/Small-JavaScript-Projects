051dcb5d74c795231306461ad3142c02a547a738
Adriana <am20dipi@alum.siena.edu>
2021-09-01 16:50:12 -0400

    Working on counting functionality; switching to setInterval()

--- counter/Counter.js
+++ counter/Counter.js
@@ -1,17 +1,21 @@
-const counter = document.querySelector('.counter')
 let count = 0
 
-document.querySelector('.start').addEventListener("click", this.handleClick)
-document.querySelector('.stop').addEventListener("click", this.handleClick)
+const startButton = document.querySelector('.start')
+const stopButton = document.querySelector('.stop')
+const resetButton = document.querySelector('.reset')
+const counter = document.querySelector('.counter')
 
+startButton.addEventListener("click", () => {
+    const seconds = setInterval(() => {
+        count => count + 1
+    }, 1000)
+    
+    document.querySelector('.start').setAttribute("disabled", "true")
+    document.querySelector('.stop').removeAttribute("disabled")
 
+    counter.innerText = seconds
+})
 
-handleClick = (e) => {
-    if (e.target.innerText == 'start'){
-        count++
-    } else if (e.target.innerText == 'stop'){
-        count--
-    } else {
-        count = 0
-    }
-}
+stopButton.addEventListener("click", ()=> {
+    clearInterval(0)
+})
