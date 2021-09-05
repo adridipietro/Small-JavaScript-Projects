f9726471c7d4863523cf0a2adfcbe9bfe814d798
Adriana <am20dipi@alum.siena.edu>
2021-09-05 17:13:33 -0400

    Creating dark + light mode on click

---
+++ dark + light mode/app.js
@@ -0,0 +1,29 @@
+const dark = document.querySelector('.dark')
+const light = document.querySelector('.light')
+const reset = document.querySelector('.reset')
+const title = document.querySelector('.title')
+
+
+function changeBackground(color){
+    document.body.style.backgroundColor = color
+}
+
+function changeTitleColor(color){
+    title.style.color = color
+}
+
+dark.addEventListener("click", () => { 
+    changeBackground('black')
+    changeTitleColor('white')
+ })
+
+
+light.addEventListener("click", () => { 
+    changeBackground('white') 
+    changeTitleColor('black')
+})
+
+reset.addEventListener("click", () => { 
+    changeBackground('yellow') 
+    changeTitleColor('black')
+})
