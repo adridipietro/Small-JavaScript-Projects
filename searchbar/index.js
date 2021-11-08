0428c767549c019989aa4c7ebd8ecbf6472bd6fa
Adriana <am20dipi@alum.siena.edu>
2021-11-08 13:48:24 -0500

    Adding notes to my index.js file explaining my code; building out foundation for a few functions: clearList(), noResults(), setList()

--- searchbar/index.js
+++ searchbar/index.js
@@ -1,3 +1,4 @@
+// creating basic data to be my search results in an object
 const people = [
     { name: 'adri'},
     { name: 'becky'},
@@ -27,4 +28,41 @@ const people = [
     { name: 'zack'},
 ]
 
+// declaring and assigning variables to the HTML elements based on their ID value
 const searchInput = document.getElementById('search')
+const list = document.getElementById('list')
+
+// "people" but filtered based on search criteria
+// pastes search result to page visibly
+function setList(results){
+    clearList()
+    for (const person of results){
+        const resultItem = document.createElement('li')
+    }
+
+    if (results.length === 0 ){
+        noResults()
+    }
+}
+
+// removes list from page
+function clearList(){
+
+}
+
+// what happens if there are no results
+function noResults(){
+
+}
+
+
+// creating event listener that listens to an "input" event
+// setting an anonymous callback function with a parameter "e" the event
+searchInput.addEventListener("input", (e) => {
+    // below for testing reasons
+    console.log('input event fired')
+    console.log(e.target.value)
+
+    // declaring and assigning the value of the event's target AKA whatever is typed in the search bar
+    const value = e.target.value
+})
