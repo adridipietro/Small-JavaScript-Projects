1e231eccd7dbc588118123b67babbcd32a49fd7a
Adriana <am20dipi@alum.siena.edu>
2021-11-09 13:11:42 -0500

    Adding regex for ensuring input contains roman numerals only

--- romanNumeralConverter/index.js
+++ romanNumeralConverter/index.js
@@ -1,16 +1,25 @@
-const results = document.querySelector("#results")
+//const results = document.getElementById("results")
 const submitButton = document.querySelector(".submit")
+
+const isRoman = (string) => {
+    // regex pattern
+    const pattern = /^(M{1,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})|M{0,4}(CM|C?D|D?C{1,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})|M{0,4}(CM|CD|D?C{0,3})(XC|X?L|L?X{1,3})(IX|IV|V?I{0,3})|M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|I?V|V?I{1,3}))$/
+    return pattern.test(string);
+};
 
 
 submitButton.addEventListener("click", () => {
     const input = document.querySelector("#roman-numeral").value
     
-    if (input.toUpperCase().includes("I", "V", "X", "L", "C", "D", "M")){
+    if (input.isRoman()){
+        input.toUpperCase()
         let array = input.split("")
         let total = 0
     
         for (let i = 0; i < array.length; i++){
-            
+            const item = array[i]
+            console.log(item)
+            console.log(total)
         }
     } else {
         return 'Sorry!'
